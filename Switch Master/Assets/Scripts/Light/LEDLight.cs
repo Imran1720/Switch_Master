60c1cffbefba409e487c06487fd59de174043ba6
Imran1720 <skimran1720@gmail.com>
2025-06-15 22:31:40 +0530

    Implmented bulb glow on switch on and off when switched off

--- Switch Master/Assets/Scripts/Light/LEDLight.cs
+++ Switch Master/Assets/Scripts/Light/LEDLight.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class LEDLight : MonoBehaviour
 {
+    [Header("Light Image")]
     [SerializeField] private Image targetLED;
+
+    [Header("Light Glow")]
+    [SerializeField] private Image glowSource;
+
+    [Header("Light Color")]
     [SerializeField] private Color lightOnColor;
     [SerializeField] private Color lightOffColor;
 
@@ -21,5 +28,6 @@ public class LEDLight : MonoBehaviour
     {
         isLightOn = !isLightOn;
         targetLED.color = isLightOn ? lightOnColor : lightOffColor;
+        glowSource.enabled = isLightOn;
     }
 }
